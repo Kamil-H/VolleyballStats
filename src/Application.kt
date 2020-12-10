74b2d1d0c48c7a465d0d6f039afc8f32e47ce860
Kamil Halko <k.halko@nomtek.com>
2020-12-10 22:17:26 +0100

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,44 @@
+package com.kamilh
+
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        json(
+            contentType = ContentType.Application.Json,
+            json = Json {
+                prettyPrint = true
+            }
+        )
+    }
+
+    routing {
+        get("/") {
+            call.respond(User(12313, "Kamil"))
+        }
+
+        install(StatusPages) {
+            exception<Throwable> { cause ->
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+    }
+}
+
+@Serializable
+data class User(val id: Long, val name: String)
