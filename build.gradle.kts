16ea8b09869a108e765c6b0c51db09bd0e630bdf
Kamil Halko <kamilhalko@gmail.com>
2022-10-28 07:25:59 +0200

    Reorgenize plugin definitions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,18 +7,3 @@ allprojects {
         repositories.applyDefault()
     }
 }
-
-buildscript {
-    repositories.applyDefault()
-    // Defining plugins here let me omit versions in "plugins" block in each build.gradle file
-    dependencies {
-        classpath(libs.plugin.kotlin)
-        classpath(libs.plugin.sqldelight)
-        classpath(libs.plugin.serialization)
-        classpath(libs.plugin.ksp)
-        classpath(libs.plugin.shadow)
-        classpath(libs.plugin.kover)
-        classpath(libs.plugin.detekt)
-        classpath(libs.plugin.jetbrains.compose)
-    }
-}
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,7 @@
 repositories {
     google()
     mavenCentral()
+    gradlePluginPortal()
 }
 
 plugins {
@@ -10,7 +11,14 @@ plugins {
 
 dependencies {
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+    // All of those plugin will be available in project's classpath
     implementation(libs.plugin.kotlin)
     implementation(libs.plugin.android)
+    implementation(libs.plugin.sqldelight)
+    implementation(libs.plugin.serialization)
+    implementation(libs.plugin.ksp)
+    implementation(libs.plugin.shadow)
+    implementation(libs.plugin.kover)
     implementation(libs.plugin.detekt)
+    implementation(libs.plugin.jetbrains.compose)
 }
