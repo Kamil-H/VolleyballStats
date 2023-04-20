c5645cb426b60ff2a9ee83298bb0e8a965c8d960
Kamil Halko <kamilhalko@gmail.com>
2023-04-20 08:43:22 +0200

    Add iOS app

---
+++ clients/ios-compose/VolleyballStats-compose/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolleyballStats-compose
+//
+//  Created by Kamil Halko on 20/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clients/ios-compose/VolleyballStats-compose/VolleyballStats_composeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballStats_composeApp.swift
+//  VolleyballStats-compose
+//
+//  Created by Kamil Halko on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct VolleyballStats_composeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
