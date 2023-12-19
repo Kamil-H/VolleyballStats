3300035fa0e7a4bdd58adfb73fda81180ba12239
Kamil Halko <kamilhalko@gmail.com>
2023-12-19 21:11:34 +0100

    Add iOS' SwiftUI app

---
+++ clients/ios-swiftui/VolleyballStats-SwiftUI/VolleyballStats_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballStats_SwiftUIApp.swift
+//  VolleyballStats-SwiftUI
+//
+//  Created by Kamil Halko on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct VolleyballStats_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
