1cce5b1a4af553736b1bf1928381616df334fe91
Kamil Halko <kamilhalko@gmail.com>
2024-01-11 21:33:00 +0100

    Fix Table crash

--- clients/shared/ui/src/commonMain/kotlin/com/kamilh/volleyballstats/ui/components/Table.kt
+++ clients/shared/ui/src/commonMain/kotlin/com/kamilh/volleyballstats/ui/components/Table.kt
@@ -1,16 +1,35 @@
 package com.kamilh.volleyballstats.ui.components
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -208,7 +227,7 @@ private fun TableRow(
     cellContent: @Composable TableRowScope.() -> Unit,
 ) {
     val currentMaxHeight = rememberSaveable { mutableStateOf(0) }
-    val indexes = remember { RowIndexes(columnCount = columnCount, stickyRowItemsCount = stickyRowItemsCount) }
+    val indexes = remember(columnCount) { RowIndexes(columnCount = columnCount, stickyRowItemsCount = stickyRowItemsCount) }
     Row {
         Box {
             TableRow(
